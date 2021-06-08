e3ad809253f64b67c75298058377fc6ef56d3ad4
nutboy <nutboy@126.com>
2021-06-08 22:56:43 +0800

    anychange

--- JupyterNoteBookComplete.sh
+++
@@ -1,4 +0,0 @@
-pip install jupyter_contrib_nbextension
-jupyter contrib nbextension install --user
-pip install --user jupyter_nbextensions_configurator
-jupyter nbextensions_configurator enable --user
